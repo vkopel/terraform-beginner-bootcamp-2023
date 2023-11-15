946ade1fded7ec6fc3a8c7cbfa63769a88c3fc5b
vkopel <145416515+vkopel@users.noreply.github.com>
2023-11-14 23:44:06 -0500

    #13 migrate local state to terraform cloud using terraform cloud block (#14)

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "vkopel"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+
   required_providers {
     random = {
       source = "hashicorp/random"
