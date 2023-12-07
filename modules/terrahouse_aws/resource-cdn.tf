177fdc70391ab710bb521c4293ae19708bd92472
vkopel <145416515+vkopel@users.noreply.github.com>
2023-12-06 23:49:03 -0500

    32 setup content version (#35)
    * #32 only upload index.html if a content version has change.
    
    * #32 content version modifications

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -16,9 +16,9 @@ locals {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_distribution
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-    origin_id                = local.s3_origin_id
+    origin_id = local.s3_origin_id
   }
 
   enabled             = true
