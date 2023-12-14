98cc65106fd5a80c187c73da9eb98eea88b47b58
Valdano Djonkou <valdanokopel@outlook.com>
2023-12-14 18:18:25 +0000

    #52 test our terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -15,20 +15,20 @@
  }
 
  provider "terratowns" {
-  endpoint = "https://terratowns.cloud/api"
-  user_uuid="4ad05ffc-cf7f-4381-b954-b9c3c6b9d6d1" 
-  token="d8d61147-3b70-4b94-ba36-0f3b7159c730"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  assets_path = var.assets_path
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
+  content_version = var.content_version
+}
 
 resource "terratowns_home" "home" {
   name = "How to be a Cloud Engineer in 2024!"
@@ -36,8 +36,8 @@ resource "terratowns_home" "home" {
 My best recommendation for your journey to be a Cloud Engineer is to 
 follow Andrew Brown's Tutorials. 
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fhjq3gz.cloudfront.net"
   town = "missingo"
   content_version = 1
 }
